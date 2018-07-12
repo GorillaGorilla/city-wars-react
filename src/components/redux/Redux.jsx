e9b327a391716366127066701c4550116711691f
Tom Geoffrey Hobbs <tomhobbs@Toms-MBP.lan>
2018-07-12 10:38:47 +0100

    redux dev-tools

--- src/components/redux/Redux.jsx
+++ src/components/redux/Redux.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import createSagaMiddleware from 'redux-saga';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router';
+import { composeWithDevTools } from 'redux-devtools-extension'; // eslint-disable-line
+
 import reducers from '../../reducers';
 import App from '../App';
 
@@ -11,7 +13,7 @@ import App from '../App';
 const history = createBrowserHistory();
 export const sagaMiddleware = createSagaMiddleware();
 
-const createStoreWMiddleware = compose(applyMiddleware(routerMiddleware(history), sagaMiddleware));
+const createStoreWMiddleware = composeWithDevTools(applyMiddleware(routerMiddleware(history), sagaMiddleware));
 const store = createStore(connectRouter(history)(reducers), createStoreWMiddleware);
 export const { dispatch } = store;
 const Redux = () => (
